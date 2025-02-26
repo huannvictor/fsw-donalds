795c7224e2c637e1054c3ccf9dbd4a5109543db6
Huann <huannvictor@gmail.com>
2025-02-26 16:20:57 -0300

    feat: adiciona componente ProductMenuHeader para exibir cabeçalho do menu de produtos

---
+++ src/app/[slug]/menu/[productId]/components/header.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import type { Product, Restaurant } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface ProductMenuHeaderProps {
+  product: Pick<Product, "name" | "imageUrl">;
+}
+
+const ProductMenuHeader = ({ product }: ProductMenuHeaderProps) => {
+  const router = useRouter();
+  const handleBackClick = () => router.back();
+
+  return (
+    <div className="relative h-[250px] w-full">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute top-4 left-4 z-50 rounded-full"
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute top-4 right-4 z-50 rounded-full"
+      >
+        <ScrollTextIcon />
+      </Button>
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-cover"
+      />
+    </div>
+  );
+};
+
+export default ProductMenuHeader;
