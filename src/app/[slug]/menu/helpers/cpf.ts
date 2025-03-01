3a08c6c418829ce6689bfc161389263aebd76cdc
Huann Almeida <huannvictor@gmail.com>
2025-03-01 16:38:58 -0300

    feat: adiciona função para remover pontuação do CPF

--- src/app/[slug]/menu/helpers/cpf.ts
+++ src/app/[slug]/menu/helpers/cpf.ts
@@ -1,3 +1,7 @@
+export const RemoveCPFPunctuation = (cpf: string) => {
+  return cpf.replace(/[\.\-]/g, "");
+};
+
 export const isValidCPF = (cpfFormForm: string): boolean => {
   const cpf = cpfFormForm.replace(/\D/g, "");
 
